ed4dda421465790d11bf75e605bf36a0e9d38b43
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2022-12-30 21:00:57 +0100

    Add files via upload

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -11,33 +11,62 @@ export const Nav = (props) => {
     const location = useLocation()
 const recetaid = useSelector(state => state.recetaid)
 
-    return (
-<div>
-        <nav className={style.header}>
-         <div>
-            {location.pathname === '/home' ? <button className={style.button2}>Inicio</button> : 
-           <Link to="/home"> <button className={style.button}>Inicio</button></Link>}
-            {location.pathname === '/create' ? <button className={style.button2}>Crear receta</button> : 
-           <Link to="/create"> <button className={style.button}>Crear receta</button></Link>}
+return (
 
-            {location.pathname === '/detail/' + recetaid.id  ? <button className={style.button2}>Informacion</button> : 
-           <button className={style.button}>Informacion</button>}
-           </div>
-           
-            {!celular ?            <><Link to="/home"> <button className={style.celular3}>Inicio</button></Link><Link to="/create"> <button className={style.celular3}>Crear receta</button></Link></> : <p>
-            </p>}
-           {location .pathname === "/home" ? !celular ?
-            <button className={style.celular} onClick={() => setCelular(true)}>
-|||</button>:
-
-<div className={style.divcel}>
-
-<Sidebar />
-<button className={style.celular2} onClick={() => setCelular(false)}>x</button>  
-</div> 
-: <></>}
-        </nav>
-        </div>
-    
-    )
+<nav >
+<div className={style.div}>
+{
+location.pathname === '/home' ? <button className={style.button2}>Inicio</button> : 
+<Link to="/home"> <button className={style.button}>Inicio</button></Link>
 }
+ {
+ location.pathname === '/create' ? <button className={style.button2}>Crear receta</button> 
+ : 
+<Link to="/create"> <button className={style.button}>Crear receta</button></Link>
+}
+                    
+{
+location.pathname === '/detail/' + recetaid.id  ?
+<button className={style.buttoninfo2}>Informacion</button>
+: 
+<button className={style.buttoninfo}>Informacion</button>
+}
+
+{!celular ? 
+<button className={style.celular} onClick={() => setCelular(!celular)}>|||</button> 
+: <button className={style.celular} onClick={() => setCelular(!celular)}>X</button>
+}
+</div>
+
+{celular ? <Sidebar/> : null}
+</nav>
+
+
+
+    //         <nav className={style.header}>
+    //          <div>
+    //             {location.pathname === '/home' ? <button className={style.button2}>Inicio</button> : 
+    //            <Link to="/home"> <button className={style.button}>Inicio</button></Link>}
+    //             {location.pathname === '/create' ? <button className={style.button2}>Crear receta</button> : 
+    //            <Link to="/create"> <button className={style.button}>Crear receta</button></Link>}
+    
+    //             {location.pathname === '/detail/' + recetaid.id  ? <button className={style.button2}>Informacion</button> : 
+    //            <button className={style.button}>Informacion</button>}
+    //            </div>
+               
+    //             {!celular ?            <><Link to="/home"> <button className={style.celular3}>Inicio</button></Link><Link to="/create"> <button className={style.celular3}>Crear receta</button></Link></> : <p>
+    //             </p>}
+    //            {location .pathname === "/home" ? !celular ?
+    //             <button className={style.button2} onClick={() => setCelular(true)}>
+    // |||</button>:
+    
+    // <div className={style.divcel}>
+    
+    // <Sidebar />
+    // <button className={style.button2} onClick={() => setCelular(false)}>x</button>  
+    // </div> 
+    // : <></>}
+    //         </nav>
+        
+        )
+    }

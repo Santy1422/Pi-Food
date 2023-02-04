db89b690b5da8aa7365cf42fa4f4912e15fbaeb7
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-04 19:12:30 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,24 @@
-import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 
-import axios from "axios"
-import { useEffect } from 'react';
-axios.defaults.baseURL = "https://pi-food-production-3adc.up.railway.app/"
-
 function App() {
-    const responseMessage = (response) => {
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log(error);
-    };
-    useEffect(() =>{
-      login()
-    }, [])
-    const login = useGoogleLogin({
-      onSuccess: codeResponse => console.log(codeResponse),
-      flow: 'auth-code',
-    });
+  const login = useGoogleLogin({
+    onSuccess: codeResponse => console.log(codeResponse),
+    flow: 'auth-code',
+  });
 
-    return (
-        <div>
-            <h2>React Google Login</h2>
-            <br />
-            <br />
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-        </div>
-    )
+  useEffect(() => {
+    login();
+  }, []);
+
+  return (
+    <div>
+      <h2>React Google Login</h2>
+      <br />
+      <br />
+      <GoogleLogin />
+    </div>
+  );
 }
+
 export default App;

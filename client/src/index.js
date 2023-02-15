a4e47a16ca81fc37cec35dbb0ac70526be807068
Santy1422 <102694038+Santy1422@users.noreply.github.com>
2023-02-15 18:02:20 +0100

    sf

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"
+import {Provider} from "react-redux"
+import store from "./redux/store.js"
 
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
-  ,
+    </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
